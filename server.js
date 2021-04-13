28f07b3c799b262d7061abd8d265bdf3c468641a
Palash Shrivastava <35087196+BattleOfPlassey@users.noreply.github.com>
2021-04-14 02:15:44 +0530

    Update server.js
    Fix#2

--- server.js
+++ server.js
@@ -47,9 +47,12 @@ if (process.env.NODE_ENV === "production") {
 }
 
  if (process.env.NODE_ENV === 'production') {
- app.use(express.static(path.join(__dirname, 'build')));
-   app.get('/*', (req, res) => {
-   res.sendFile(path.join(__dirname, 'build', 'index.html')); 
+// app.use(express.static(path.join(__dirname, 'build')));
+  // app.get('/*', (req, res) => {
+  // res.sendFile(path.join(__dirname, 'build', 'index.html')); 
+//});
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
 }
 

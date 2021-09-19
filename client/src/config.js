f5ead1e3f41b83fcb42d17adbb4b35d55e5b79b5
Palash Shrivastava <palashshanu@gmail.com>
2021-09-20 01:27:47 +0530

    revert config.js file

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 //For GitHub Pages deployment
-// export const HOST = 'https://ministry-of-vocabulary.herokuapp.com';
+export const HOST = 'https://ministry-of-vocabulary.herokuapp.com';
 
 // For Local Server & Production deployment
-export const HOST = (process.env.env==='production') ? 'https://ministry-of-vocabulary.herokuapp.com' : ''; 
+// export const HOST = (process.env.env==='production') ? 'https://ministry-of-vocabulary.herokuapp.com' : ''; 
